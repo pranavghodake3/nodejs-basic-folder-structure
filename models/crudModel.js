3fc9ce15097c8c912921ad14f2a07c1fa303f3be
Pranav Ghodake <pranavghodake3@gmail.com>
2025-03-04 12:48:03 +0530

    Added new module and CRUD feature

--- models/crudModel.js
+++ models/crudModel.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const crudModuleNameSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    level: { type: String, required: true },
-    changeFrequency: { type: String, required: true },
-    hint: { type: String, required: true },
-    password: { type: Boolean, required: true },
 });
 
 const crudModuleName = mongoose.model("crudModuleName", crudModuleNameSchema);
